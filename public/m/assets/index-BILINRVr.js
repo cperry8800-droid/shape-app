aa7dc82d8bee3780ad2966e9d549abcc797ea2d6
Christopher Perry <cperry8800@gmail.com>
2026-06-03 16:52:02 -0400

    Settings plan card: "Activate membership" for signed-in users without a sub (#950)
    Plan card runs off signedIn + hasSub: real subscribers see Manage (billing portal); signed-in users without a Stripe sub see "Activate membership" routing to checkout (not the dead portal); browsing shows Join now.

--- public/m/assets/index-BILINRVr.js
+++
@@ -1,2 +0,0 @@
-import{r}from"./index-tOpxAf6N.js";var a=r();export{a as r};
-//# sourceMappingURL=index-BILINRVr.js.map
---
+++ public/m/assets/index-CTI8uA-C.js
@@ -0,0 +1,2 @@
+import{r}from"./index-C1ebmOt8.js";var a=r();export{a as r};
+//# sourceMappingURL=index-CTI8uA-C.js.map
