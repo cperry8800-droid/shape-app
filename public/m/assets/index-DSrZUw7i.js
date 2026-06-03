3b6408d6a6f837172fb1cd40fbac0af73290afd3
Christopher Perry <cperry8800@gmail.com>
2026-06-03 16:41:10 -0400

    Settings: Profile mode switcher only for multi-account users (#948)
    The Profile mode role switcher now only renders when the signed-in profile has more than one role, and lists only the roles the user actually created (profile.roles), not all three.

---
+++ public/m/assets/index-DSrZUw7i.js
@@ -0,0 +1,2 @@
+import{r}from"./index-DDDjqn1E.js";var a=r();export{a as r};
+//# sourceMappingURL=index-DSrZUw7i.js.map
--- public/m/assets/index-WCAz9coS.js
+++
@@ -1,2 +0,0 @@
-import{r}from"./index-CYPycSfc.js";var a=r();export{a as r};
-//# sourceMappingURL=index-WCAz9coS.js.map
